4346fcd9d69fd3e3ee0e289e1289b789bb45a45e
NvmeSensor <priyannxhuu@gmail.com>
2024-08-25 11:06:08 +0530

    fixed bug

--- src/app/components/GetinTouch.tsx
+++ src/app/components/GetinTouch.tsx
@@ -7,21 +7,21 @@ const GetInTouch = () => {
     <section className="flex justify-start items-start w-full ">
       <div className="lg:container flex flex-col justify-start items-start gap-5 w-full">
         <p className="text-xl font-normal self-start w-full">
-          Whether you’re buying, selling, or renting, we’re here to help. Reach
-          out to us, and we'll respond as soon as possible.
+          {`Whether you’re buying, selling, or renting, we’re here to help. Reach
+          out to us, and we'll respond as soon as possible.`}
         </p>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 justify-start items-center w-full">
           <div className="flex flex-col justify-center items-center text-2xl gap-2 px-5 py-3 rounded-lg cursor-pointer card">
             <FaMapMarkerAlt className="text-3xl" />
-            <p>1234 Real Estate St, City, Country</p>
+            <p>{`1234 Real Estate St, City, Country`}</p>
           </div>
           <div className="flex flex-col justify-center items-center text-2xl gap-2 px-5 py-3 rounded-lg cursor-pointer card">
             <FaEnvelope className="text-3xl" />
-            <p>info@realestate.com</p>
+            <p>{`info@realestate.com`}</p>
           </div>
           <div className="flex flex-col justify-center items-center text-2xl gap-2 px-5 py-3 rounded-lg cursor-pointer card">
             <FaPhoneAlt className="text-3xl" />
-            <p>0123456789</p>
+            <p>{`0123456789`}</p>
           </div>
         </div>
       </div>
